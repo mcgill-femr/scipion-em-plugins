551fff1f19233458726389a5a99fa3211d31652c
JOsue <josuegbl@gmail.com>
2018-08-15 10:28:02 -0400

    changed path to localize plugins

--- bash-plugins.sh
+++ bash-plugins.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-for f in /home/josem/work/development/scipion-em-plugins/scipion-em-*
+for f in /home/josuegbl/SOFTWARE/scipion-em-plugins/scipion-em-*
 do
     export PYTHONPATH=$PYTHONPATH:$f
     #echo $f
